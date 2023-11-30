1ce5a4d38e6a56cc7f9b4ebde203af5893bdbcca
danilospinola <danspinola10@gmail.com>
2023-11-30 17:37:05 -0300

    a

--- all_win/src/servicos/api.js
+++ all_win/src/servicos/api.js
@@ -3,9 +3,16 @@ import axios from "axios";
 const allWinApi = axios.create({ baseURL: `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id` });
 
 async function getPlayerById(gameId, TagLine= "Br1") {
-  const response = await allWinApi.get(`/${gameId}/${TagLine}`);
-
-  return response.data;
+  const response = await allWinApi.get(`/${gameId}/${TagLine}`, 
+  {
+    headers: {
+    'X-Riot-Token': 'RGAPI-a4dd09f7-318e-46e8-8245-5566f7cc3aa9',
+    }
+  }).then((res) => console.log(res))
+  .catch((err) => console.error(err));
+  
+  console.log(response)
+  return response;
 }
 async function postAllWin(id, data, nome, img) {
   await allWinApi.post("/", { id: id, nome: nome, data: data, imagem:img });
